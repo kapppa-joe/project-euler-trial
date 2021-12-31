0656c7ddbb28391de99e967a19b468966381623f
Joe Fong <kapppa.joe@gmail.com>
2021-12-31 12:42:45 +0000

    add solution to problem 043

--- tests/test_p041_050.py
+++ tests/test_p041_050.py
@@ -2,6 +2,8 @@ import pytest
 
 from p041_050.p041 import find_largest_pandigital_prime
 from p041_050.p042 import is_triangle_number, word_value
+from p041_050.p043 import satisfy_divisible_properties
+from util import int_to_digits
 
 
 def test_p041_find_largest_pandigital_prime():
@@ -38,3 +40,11 @@ test_cases_p042_is_triangle_num = [
 @pytest.mark.parametrize("num, expected_output", test_cases_p042_is_triangle_num)
 def test_p042_is_triangle_number(num, expected_output):
     assert is_triangle_number(num) == expected_output
+
+
+def test_p043_satisfy_divisible_properties():
+    assert satisfy_divisible_properties((1, 4, 0, 6, 3, 5, 7, 2, 8, 9)) == True
+    assert satisfy_divisible_properties(
+        (1, 4, 0, 6, 3, 5, 7, 2, 8, 8)) == False
+    for num in [1406357289, 1430952867, 1460357289, 4106357289, 4130952867, 4160357289]:
+        assert satisfy_divisible_properties(tuple(int_to_digits(num))) == True
